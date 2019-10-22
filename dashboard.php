c494bc7e428750cb372e8810fe547c9c5d72cecb
onixalaikal <55238484+onixalaikal@users.noreply.github.com>
2019-10-22 12:50:49 +0700

    Add files via upload

---
+++ dashboard.php
@@ -0,0 +1,107 @@
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<meta name="viewport" content="width=device-width, maximum-scale=5,
+minimum-scale=0.5"/>
+<title>HOME</title>
+<link href="5.css" rel="stylesheet" type="text/css" />
+<link href="tanggal.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+<div id="kepala">
+<div id="mata"><img src="uks.png" width="80" height="80"></div>
+<div id="hidung">
+<font size="4" color="#ffffff">E-PPUKSKENSA</font>
+</div>
+<div id="mulut">
+<font size="4" color="#ffffff">UKS SKENSA PATTIEN SERVICE ONLINE </font>
+</div>
+</div>
+</div>
+<div id="tangan">
+<ul>
+<li><a href="#">HOME</a></li>
+<li><a href="#">PASIEN</a></li>
+<li><a href="#">OBAT</a></li>
+<li><a href="#">LAPORAN</a></li>
+<li><a href="#">ABOUT</a></li>
+</ul>
+</div>
+<div id="awak">
+<h2>WELCOME DASHBOARD</h2>
+<div id="caleandar">
+<div id="calendar-control">
+  <div id="monthNow">Januari 2014</div>
+  <div id="nextMonth"></div>
+  <div id="prevMonth"></div>
+ </div>
+ <div id="dayNames">
+  <ul>
+   <li>Minggu</li>
+   <li>Senin</li>
+   <li>Selasa</li>
+   <li>Rabu</li>
+   <li>Kamis</li>
+   <li>Jum'at</li>
+   <li>Sabtu</li>
+  </ul>
+ </div>
+ <div id="daysNum">
+ <canvas id="myCanvas"></canvas>
+<script type="text/javascript" src="script2.js"></script>
+ </div>
+</div>
+<div id="kanan">
+<TABLE>
+<tr>
+<th>TANGGAL</th>
+<th>EVENT</th>
+<th>LOKASI</th>
+<th>JAM</th>
+</tr>
+<tr>
+<td>31-08-2019</td>
+<td>DONOR DARAH</td>
+<td>UKS</td>
+<td>08.00-13.00</td>
+</tr>
+<tr>
+<td>01-12-2019</td>
+<td>PROGRAM KACAMATA GRATIS</td>
+<td>UKS</td>
+<td>11.00-12.30</td>
+</tr>
+<tr>
+<td>10-11-2019</td>
+<td>SOSIALISASI ANEMIA</td>
+<td>RUANG KELAS</td>
+<td>JAM PRAMUKA</td>
+</tr>
+<tr>
+<td>28-11-2019</td>
+<td>VAKSINASI</td>
+<td>UKS</td>
+<td>08.00-12.00</td>
+</tr>
+</table>
+</div>
+<div id="bawah">
+JUMLAH PASIEN :
+<h1>05</h1>
+</div>
+<div id="jeruh">
+GAMBAR PASIEN
+<img src="pasien.png" width="150" height="130">
+</div>
+</div>
+<div id="aurat">
+<marquee behavior="alternate"><font color="#ffffff"><b>PELAYANAN UKS DIMULAI DARI JAM 08.00 SAMPAI DENGAN 14.00 WIB</b></font></marquee>
+</div>
+<div id="kaki">
+<hr color="#00ff00">
+<img src="instagram.png" width="50" height="50" align="left">
+<img src="gmail.png" width="50" height="50" align="right">
+<h6>THE INFORM AT GMAIL uks@skensa.ac.id OR INSTAGRAM uksskensa</h6>
+</div>
+</body>
+</html>
